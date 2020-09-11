011d9f39ea005839f79dcdbb4e7229c96219341b
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-11 11:46:28 +0900

    ユーザー登録のバリデーション を再度修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :birthday, presence: true
-
+  validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true,
              format: { with: /\A\S+@\S+\.\S+\z/ , message: 'には@が必要です' }
-
   PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,}\z/
   validates :password, presence: true,
              format: { with: PASSWORD_REGEX , message: 'には英字と数字の両方を含めて設定してください' }
-
   validates :last_name, :first_name, presence: true, 
              format: { with: /\A[ぁ-んァ-ン一-龥]/ , message: 'は全角で入力してください' }
-
   validates :last_name_kana, :first_name_kana, presence: true,
              format: { with: /\A[ァ-ヶー－]+\z/ , message: 'は全角カタカナで入力してください' }
+  validates :birthday, presence: true
 end
