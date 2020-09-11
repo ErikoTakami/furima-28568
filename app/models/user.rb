baa58a875539b6c6ccaec24ea205b7fa6851ab04
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-11 23:43:17 +0900

    Rubocopによるコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true,
-             format: { with: /\A\S+@\S+\.\S+\z/ , message: 'は@を含む必要があります' }
-  PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/
+                    format: { with: /\A\S+@\S+\.\S+\z/, message: 'は@を含む必要があります' }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/.freeze
   validates :password, presence: true,
-             format: { with: PASSWORD_REGEX , message: 'には英字と数字の両方を含めて設定してください' }
-  validates :last_name, :first_name, presence: true, 
-             format: { with: /\A[ぁ-んァ-ン一-龥]/ , message: 'は全角で入力してください' }
+                       format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
+  validates :last_name, :first_name, presence: true,
+                                     format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
   validates :last_name_kana, :first_name_kana, presence: true,
-             format: { with: /\A[ァ-ヶー－]+\z/ , message: 'は全角カタカナで入力してください' }
+                                               format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
   validates :birthday, presence: true
 end
