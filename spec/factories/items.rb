759fa488450dd3e5b4c6c648fe9a37d55ba42de3
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-14 00:59:07 +0900

    FactoryBotでテスト用のインスタンスを定義

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
   factory :item do
+    name                 { Faker::Games::Pokemon.name }
+    text                 { Faker::Lorem.sentence }
+    category             { Faker::Number.between(from: 1, to: 10) }
+    status               { Faker::Number.between(from: 1, to: 6) }
+    shipping_charges     { Faker::Number.between(from: 1, to: 2) }
+    shipping_origin      { Faker::Number.between(from: 1, to: 47) }
+    days_until_shipping  { Faker::Number.between(from: 1, to: 3) }
+    price                { Faker::Number.between(from: 300, to: 9999999) }
   end
 end
