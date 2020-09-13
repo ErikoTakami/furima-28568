2162e3395b5fd9941913faed988045c5fce0f996
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-13 14:53:57 +0900

    itemsテーブルとimageカラムのアソシエーションを設定
    READMEのitemsテーブルのアソシエーションにhas_one_attached :imageを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category
+    validates :status
+    validates :shipping_charges
+    validates :shipping_origin
+    validates :days_until_shipping
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+  end
 end
