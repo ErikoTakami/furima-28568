44c295770f2f9e35dbfcc47fb02bbd4a9da05165
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-13 12:35:37 +0900

    itemsテーブルの作成

--- db/migrate/20200913032529_create_items.rb
+++ db/migrate/20200913032529_create_items.rb
@@ -5,8 +5,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :text,                   null: false
       t.integer :category,            null: false
       t.integer :status,              null: false
-      t.integer :shipping_charges,     null: false
-      t.integer :shipping_origin,      null: false
+      t.integer :shipping_charges,    null: false
+      t.integer :shipping_origin,     null: false
       t.integer :days_until_shipping, null: false
       t.integer :price,               null: false
       t.references :user,             null: false, foreign_key: true
