3ba01cf6927c5c64a5353b9b30f69eb611d2e6a1
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-13 15:06:20 +0900

    ストロングパラメーターの設定
    imageも保存できるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new
+    @item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :category, :status, :shipping_charges, :shipping_origin, :days_until_shipping, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  
+
   with_options presence: true do
     validates :name
     validates :text
@@ -11,5 +11,6 @@ class Item < ApplicationRecord
     validates :shipping_origin
     validates :days_until_shipping
     validates :price, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+    validates :image
   end
 end
