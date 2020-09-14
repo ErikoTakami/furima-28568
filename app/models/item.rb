72714678b220d99d104fd629f22193ccf2ccdddf
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-14 22:40:59 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text
-    validates :category
-    validates :status
-    validates :shipping_charges
-    validates :shipping_origin
-    validates :days_until_shipping
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+    validates :category,            numericality: { other_than: 0 }
+    validates :status,              numericality: { other_than: 0 }
+    validates :shipping_charges,    numericality: { other_than: 0 }
+    validates :shipping_origin,     numericality: { other_than: 0 }
+    validates :days_until_shipping, numericality: { other_than: 0 }
+    validates :price,               numericality: { only_integer: true, message: 'は半角数字で入力してください' }
     validates :image
   end
+  validates :price,  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
