3645352b1397392dce9d73ea3f6df9a4be60dafc
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-14 11:08:31 +0900

    FactoryBotにアソシエーション、テスト画像を追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :item do
+    association :user
     name                 { Faker::Games::Pokemon.name }
     text                 { Faker::Lorem.sentence }
     category             { Faker::Number.between(from: 1, to: 10) }
@@ -8,5 +9,9 @@ FactoryBot.define do
     shipping_origin      { Faker::Number.between(from: 1, to: 47) }
     days_until_shipping  { Faker::Number.between(from: 1, to: 3) }
     price                { Faker::Number.between(from: 300, to: 9999999) }
+  
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/item-sample.png'), filename: 'item-sample.png' )
+    end
   end
 end
