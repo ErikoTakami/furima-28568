0860befb99e11c84be348a7d3e5b05fc6bba0bf1
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-15 14:43:04 +0900

    rubocopによるコード修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,5 +13,5 @@ class Item < ApplicationRecord
     validates :price,               numericality: { only_integer: true, message: 'は半角数字で入力してください' }
     validates :image
   end
-  validates :price,  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
