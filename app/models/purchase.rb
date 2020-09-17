751e989f85c5cb9d2f8ec26b5396a79fad36e5df
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-17 14:25:37 +0900

    ActiveHashを用いてprefectureカラムを作成

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,7 @@
 class Purchase < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :item
   has_one :shipping_address
+  belongs_to_active_hash :prefecture
 end
