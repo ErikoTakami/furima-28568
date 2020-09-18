1af90bb609322b9571d795b43aa762ff0329739e
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-18 21:09:11 +0900

    カード情報をattr_accesorで設定

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -1,13 +1,13 @@
 class ItemPurchase
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :block, :building, :phone_number, :token, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :block, :building, :phone_number, :token, :user_id, :item_id, :number, :cvc, :exp_month, :exp_year
 
   with_options presence: true do
-    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
     validates :block
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ , message: 'is invalid'}
     validates :token
   end
 
