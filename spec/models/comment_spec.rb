4afa101862cd068d0ed2cabfb141f674fd4b1161
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-21 22:04:27 +0900

    commentモデルの単体テストの実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント機能' do
+    context 'コメントが投稿できるとき' do
+      it 'コメントが入力されていれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントが投稿できないとき' do
+      it 'コメントが入力されていないと投稿できない' do
+        @comment.comment = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("コメントを入力してください")
+      end
+    end
+  end
 end
